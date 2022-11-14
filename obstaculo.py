d532128ec124330ba48fafd38ac36927bcf8bb6d
asaPUP <asael.silvam@gmail.com>
2022-11-14 14:04:59 -0800

    su

---
+++ obstaculo.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Obstaculo(pygame.sprite.Sprite):
+    def __init__(self, x, y, tipo):
+        super().__init__()
+
+        self._x = x
+        self._y = y
+
+        self._tipo = tipo
+    
+    def collission():
+        pass
--- picos.py
+++ picos.py
@@ -1,27 +1,20 @@
 import pygame
+from obstaculo import Obstaculo
 
-class Picos(pygame.sprite.Sprite):
-    def __init__(self, x, y,tipo, dalt):
+class Picos(Obstaculo):
+    def __init__(self):
         super().__init__()
 
-        self.x = x
-        self.y = y
-
-        if tipo == 'rosales':
-            if dalt == True:
-                self.image = pygame.image.load('assets/Rosales/rosalesDAL.png').convert_alpha() # Carga imagen
-                self.image = pygame.transform.scale2x(self.image) # Escala imagen a 2x
-            else:
-                self.image = pygame.image.load('assets/Rosales/rosales.png').convert_alpha() # Carga imagen
-                self.image = pygame.transform.scale2x(self.image) # Escala imagen a 2x
+        if self._tipo == 1:
+            self._image = pygame.image.load('assets/Enredaderas/enredadera.png').convert_alpha() # Carga imagen
+            self._image = pygame.transform.scale2x(self.image) # Escala imagen a 2x
             
-        elif tipo == 'enredadera':
-            if dalt == True:
-                self.image = pygame.image.load('assets/Enredaderas/enredaderaDAL.png').convert_alpha() # Carga imagen
-                self.image = pygame.transform.scale2x(self.image) # Escala imagen a 2x
-            else:
-                self.image = pygame.image.load('assets/Enredaderas/enredadera.png').convert_alpha() # Carga imagen
-                self.image = pygame.transform.scale2x(self.image) # Escala imagen a 2x           
-        
-        #self.image = pygame.image.load('assets/Sprite-0006.png').convert_alpha()
-        self.rect = self.image.get_rect(midbottom = (self.x, self.y))
+        else:
+            self._image = pygame.image.load('assets/Rosales/rosales.png').convert_alpha() # Carga imagen
+            self._image = pygame.transform.scale2x(self.image) # Escala imagen a 2x        
+
+        self._rect = self.image.get_rect(midbottom = (self.x, self.y))
+    
+    def collision(self, jugador):
+        if self.rect.colliderect(jugador.rect):
+            return True
